e93a14de2ffb9bf488c56ee7716ac6b428fdcfe7
Ivan Paraskun <ig.paraskun@gmail.com>
2026-08-15 18:31:34 +0700

    git

--- home/paraskun/programs/git.nix
+++ home/paraskun/programs/git.nix
@@ -8,6 +8,11 @@
       user = {
         name = "Ivan Paraskun";
         email = "ig.paraskun@gmail.com";
+
+        signing = {
+          key = "0x54758D6FE627CB11";
+          signByDefault = true;
+        };
       };
     };
   };
