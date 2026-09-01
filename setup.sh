1970b311a6c2cc2986972c28b4181f4aa461f51c
Ivan Paraskun <ig.paraskun@gmail.com>
2026-09-01 21:55:54 +0700

    upd

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install \
-  | sh -s -- --no-daemon --extra-conf "sandbox = false"
+  | sh -s -- --no-daemon
 
 nix --extra-experimental-features "nix-command flakes" \
   run home-manager -- switch --flake .#$1
