9f7ea75df3ee79ab7d7b8baf159cc4da4c42b599
Ivan Paraskun <ig.paraskun@gmail.com>
2026-09-02 22:27:11 +0700

    upd

--- flake.nix
+++ flake.nix
@@ -23,13 +23,29 @@
 
   outputs = { self, nixpkgs, nixpkgs-darwin, home-manager, nix-darwin, rftp }@inputs: {
     homeConfigurations = {
-      ltp-n = home-manager.lib.homeManagerConfiguration {
+      "ltp-n@amd" = home-manager.lib.homeManagerConfiguration {
+      	pkgs = nixpkgs.legacyPackages.x86_64-linux;
         modules = [
           ./home/paraskun/cont/ltp-n
         ];
       };
 
-      ubuntu = home-manager.lib.homeManagerConfiguration {
+      "ltp-n@arm" = home-manager.lib.homeManagerConfiguration {
+      	pkgs = nixpkgs.legacyPackages.aarch64-linux;
+        modules = [
+          ./home/paraskun/cont/ltp-n
+        ];
+      };
+
+      "ubuntu@amd" = home-manager.lib.homeManagerConfiguration {
+      	pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        modules = [
+          ./home/paraskun/cont/ubuntu
+        ];
+      };
+
+      "ubuntu@arm" = home-manager.lib.homeManagerConfiguration {
+      	pkgs = nixpkgs.legacyPackages.aarch64-linux;
         modules = [
           ./home/paraskun/cont/ubuntu
         ];
