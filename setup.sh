cd2b964fb3a58653ca111495bcda1a3568eb2507
Ivan Paraskun <ig.paraskun@gmail.com>
2026-09-02 22:44:00 +0700

    upd

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-if [[ ! -d "/nix" ]]; then
+if [ ! -d "/nix" ]; then
   curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install \
     | sh -s -- --no-daemon
 fi
 
 . ~/.nix-profile/etc/profile.d/nix.sh
 
-nix --extra-experimental-features "nix-command flakes" \
+NIX_CONFIG="experimental-features = nix-command flakes" nix \
   run home-manager -- switch --flake .#$1
